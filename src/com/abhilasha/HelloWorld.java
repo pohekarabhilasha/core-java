749ae0a9a771ea1e752eba056c8e68a52817b9ef
Abhilasha Pohekar <pohekarabhilasha@gmail.com>
2025-09-03 14:16:01 +0100

    data types

---
+++ src/com/abhilasha/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.abhilasha;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World !!!");
+
+		byte b = (byte) 127;
+		boolean bool = false;
+		char c = 'a';
+		short s = 234;
+		int i = 289;
+		float f = 54.888887f;
+		long l = 299;
+		double d = 23.898945378;
+
+		System.out.println(b);
+		System.out.println(bool);
+		System.out.println(c);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(l);
+		System.out.println(d);
+	}
+}
